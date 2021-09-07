27710e09e7c14fbddc18d4d2937fdcd9922a8c6e
anavitbel <60670382+anavitbel@users.noreply.github.com>
2021-09-07 10:54:52 +0300

    Create squareseqdigit.py

---
+++ 3 курс/Программирование/lab1/squareseqdigit.py
@@ -0,0 +1,18 @@
+import math
+
+
+def squareSequenceDigit(n):
+
+    if (n == 1):
+        return 1
+    else:
+        return n
+
+
+if __name__ == "__main__":
+    print(squareSequenceDigit(1))
+    print(squareSequenceDigit(2))
+    print(squareSequenceDigit(7))
+    print(squareSequenceDigit(12))
+    print(squareSequenceDigit(17))
+    print(squareSequenceDigit(27))
